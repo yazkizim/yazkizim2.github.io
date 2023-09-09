211831c37d7b982f79aac6763ba7b5b0eb0d4a8d
yazkizim <100561777+yazkizim@users.noreply.github.com>
2023-09-10 01:19:47 +0300

    rp

--- sinav/quiz.js
+++ sinav/quiz.js
@@ -77,6 +77,17 @@ function render(quiz_opts) {
 
 }
 
+function shuffle(sourceArray) {
+  for (var i = 0; i < sourceArray.length - 1; i++) {
+      var j = i + Math.floor(Math.random() * (sourceArray.length - i));
+
+      var temp = sourceArray[j];
+      sourceArray[j] = sourceArray[i];
+      sourceArray[i] = temp;
+  }
+  return sourceArray;
+}
+
 function createTest(){
   // list of questions to insert into quiz
   
@@ -353,16 +364,7 @@ function resultsText(state) {
 
 }
 
-function shuffle(sourceArray) {
-  for (var i = 0; i < sourceArray.length - 1; i++) {
-      var j = i + Math.floor(Math.random() * (sourceArray.length - i));
 
-      var temp = sourceArray[j];
-      sourceArray[j] = sourceArray[i];
-      sourceArray[i] = temp;
-  }
-  return sourceArray;
-}
 
 
 })(jQuery);
