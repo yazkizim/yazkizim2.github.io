e9001b35393c1205c73fa83fd5336573e205d634
yazkizim <100561777+yazkizim@users.noreply.github.com>
2023-09-10 22:06:30 +0300

    kabar

--- assets/js/gokhan.js
+++ assets/js/gokhan.js
@@ -1909,6 +1909,9 @@ $(document).ready(function(){
 	} 
 
 	$('.list-group-item').on('click', function() {
+
+		
+
 		$('.glyphicon', this)
 		  .toggleClass('glyphicon-chevron-right')
 		  .toggleClass('glyphicon-chevron-down');
